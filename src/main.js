c02bc4f3c0e69ce0b33fa03b8be4e318fa7e8e10
yochered <aspizhav@e1r2p16.unit.ua>
2020-01-29 20:48:11 +0200

    day02

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueApollo from "vue-apollo";
+import ApolloClient from "apollo-boost";
+import VueTheMask from "vue-the-mask";
+Vue.use(VueTheMask);
 
-Vue.config.productionTip = false
+Vue.use(VueApollo);
+
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: "https://api.graphloc.com/graphql"
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  apolloProvider
+}).$mount("#app");
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Router from "vue-router";
+Vue.use(Router);
+export default new Router({
+	mode: "history",
+	routes: [
+		// {
+		// 	path: "/",
+		// 	name: "home",
+		// 	component: home
+		// },
+		// {
+		// 	path: "/english",
+		// 	name: "English",
+		// 	component: English
+		// },
+		// {
+		// 	path: "/russian",
+		// 	name: "Russian",
+		// 	component: Russian
+		// }
+	]
+});
